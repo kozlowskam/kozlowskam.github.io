4d7e91d780d1494eadbd8f3c7d7fb7e01e02cc9a
kozlowskam <malgorzatta.kozlowska@gmail.com>
2021-01-30 17:21:03 +0100

    Updates

---
+++ precache-manifest.8ee88dd71cc0c29cc9b3d1051190d0c8.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = [
+  {
+    "revision": "97d86745fbfa342d004f",
+    "url": "/static/css/main.26b2a521.chunk.css"
+  },
+  {
+    "revision": "97d86745fbfa342d004f",
+    "url": "/static/js/main.97d86745.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "515beb9d9e6f69ef692f",
+    "url": "/static/js/2.515beb9d.chunk.js"
+  },
+  {
+    "revision": "30d9dfe98974473162f9036b5579b161",
+    "url": "/static/media/010cover.30d9dfe9.png"
+  },
+  {
+    "revision": "47168244c85a4570bbcc5451fb9482cc",
+    "url": "/static/media/arrow-black.47168244.svg"
+  },
+  {
+    "revision": "6d8c42fac53f3087b5b5473f6b351b53",
+    "url": "/static/media/arrow.6d8c42fa.svg"
+  },
+  {
+    "revision": "3073d0e6eed3074ac44f5d94ba56ab56",
+    "url": "/static/media/github_logo-black.3073d0e6.png"
+  },
+  {
+    "revision": "993f082a8cd0d48d43e2ed0bbc1d8040",
+    "url": "/static/media/behance-logo.993f082a.png"
+  },
+  {
+    "revision": "5eb2e6df3d39c8a4208d49cb56089a76",
+    "url": "/static/media/github_logo.5eb2e6df.png"
+  },
+  {
+    "revision": "493ee0c051dcbb180170ee5c6869361f",
+    "url": "/static/media/jazz01.493ee0c0.png"
+  },
+  {
+    "revision": "7b7d79b381d14f6ca51ad962ceeb47a1",
+    "url": "/static/media/jazz02.7b7d79b3.png"
+  },
+  {
+    "revision": "dd92356baefaa91cea9d8d9239409cde",
+    "url": "/static/media/sztuk01.dd92356b.png"
+  },
+  {
+    "revision": "215e788852369706c1fb002e5a9f4dbe",
+    "url": "/static/media/sztuk02.215e7888.png"
+  },
+  {
+    "revision": "d5acd27c08af7c275201a055714bb7ff",
+    "url": "/static/media/no_01.d5acd27c.png"
+  },
+  {
+    "revision": "2a64595d8675fad146aca04e30c6ae44",
+    "url": "/static/media/no_02.2a64595d.png"
+  },
+  {
+    "revision": "5d260805db0b4a3b83b0aa02efd8e654",
+    "url": "/static/media/jaksiebiesamego.5d260805.jpg"
+  },
+  {
+    "revision": "1e46bc334c2bb16f3f5fdc6d8459dc68",
+    "url": "/static/media/edyp.1e46bc33.jpg"
+  },
+  {
+    "revision": "68fa8e36ca604923bdc13bb8316e1cde",
+    "url": "/static/media/avalanche.68fa8e36.jpg"
+  },
+  {
+    "revision": "43c754019bc42fb71cafba461fda6aa9",
+    "url": "/static/media/buffalo.43c75401.jpg"
+  },
+  {
+    "revision": "a2f444f0e63b586c2a5a6918d5844155",
+    "url": "/static/media/CPM16.a2f444f0.jpg"
+  },
+  {
+    "revision": "3a72775a0d5e255fbd77e0cc7cbf70fb",
+    "url": "/static/media/CPM17.3a72775a.jpg"
+  },
+  {
+    "revision": "7c6967cd95180b64e1089ebd80801e67",
+    "url": "/static/media/CPM18.7c6967cd.jpg"
+  },
+  {
+    "revision": "50bfff689550538c9e8dd30f701f2a40",
+    "url": "/static/media/logo_mupop.50bfff68.jpg"
+  },
+  {
+    "revision": "46e948ff9be5c3dc541daf5f5e6cfe78",
+    "url": "/static/media/logo_mgd.46e948ff.jpg"
+  },
+  {
+    "revision": "334f3decc8f8d43b508190e6d2aa36a5",
+    "url": "/static/media/logo_tm.334f3dec.jpg"
+  },
+  {
+    "revision": "1b4dc9b52852a2290570deb40a9b239c",
+    "url": "/static/media/logo_ccs.1b4dc9b5.png"
+  },
+  {
+    "revision": "6ea34ae8afb3010aa26493bbd0555581",
+    "url": "/static/media/logo_artcafe.6ea34ae8.jpg"
+  },
+  {
+    "revision": "0e3a9aecfe90d7c7e05fb377dd014f20",
+    "url": "/static/media/CPEcover.0e3a9aec.jpg"
+  },
+  {
+    "revision": "3e00ccec3845925069434a1c019dc535",
+    "url": "/static/media/stoka.3e00ccec.png"
+  },
+  {
+    "revision": "a09d0fa7ca040c4053e935442a1e6f85",
+    "url": "/static/media/cpm.a09d0fa7.jpg"
+  },
+  {
+    "revision": "c6b44b0cfc6c252cea96aaa80a9f431b",
+    "url": "/static/media/010book_in3.c6b44b0c.jpg"
+  },
+  {
+    "revision": "4fdd485cc1cf7283e8a653a5bb5b81e9",
+    "url": "/static/media/010book_in4.4fdd485c.jpg"
+  },
+  {
+    "revision": "ec3fe1fa4237e6b0e01bcdcab561307b",
+    "url": "/static/media/010book_in1.ec3fe1fa.png"
+  },
+  {
+    "revision": "5274cf15c3d5794446413bf4dfc74d7e",
+    "url": "/static/media/010book_in2.5274cf15.png"
+  },
+  {
+    "revision": "79029380722d7ceac51f978a0b24cfd9",
+    "url": "/static/media/posters.79029380.png"
+  },
+  {
+    "revision": "9853fd23b6e67784f7ffcad9a0d31024",
+    "url": "/static/media/vi_cover.9853fd23.png"
+  },
+  {
+    "revision": "70f892aad44f4d3e379700304d632936",
+    "url": "/static/media/logo_mupop.70f892aa.png"
+  },
+  {
+    "revision": "b8c5009e0b830d872a8245fd6977023d",
+    "url": "/static/media/typo.b8c5009e.png"
+  },
+  {
+    "revision": "d5e3743714c9e1ae0dec385218aeb78c",
+    "url": "/static/media/theydo_cover.d5e37437.png"
+  },
+  {
+    "revision": "804f55312ec5b9ecfefc74e94ef235a6",
+    "url": "/static/media/airmiles_cover.804f5531.png"
+  },
+  {
+    "revision": "4890f55790008cae64866e9e690f812d",
+    "url": "/static/media/audi_cover.4890f557.png"
+  },
+  {
+    "revision": "5c388aba83d95fc760d498b4f309d2bc",
+    "url": "/static/media/transformcity_cover.5c388aba.png"
+  },
+  {
+    "revision": "ea0e81e0e415bfde0c1f554b1db68652",
+    "url": "/static/media/worldstream_cover.ea0e81e0.png"
+  },
+  {
+    "revision": "9ccf5cf22a73a66158ed1d5db9b059dc",
+    "url": "/static/media/urbee_cover.9ccf5cf2.png"
+  },
+  {
+    "revision": "d8cf5733efe05e80db219c41cefe9f1d",
+    "url": "/static/media/ts_cover.d8cf5733.png"
+  },
+  {
+    "revision": "be35c96d7f769badc861d5a9c585d84b",
+    "url": "/static/media/csc_cover.be35c96d.png"
+  },
+  {
+    "revision": "eefd2c63b91badc26a605043bb4b810a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a862fc85dd173bac59293a56754a805.js"
+  "/precache-manifest.8ee88dd71cc0c29cc9b3d1051190d0c8.js"
 );
 
 workbox.clientsClaim();
